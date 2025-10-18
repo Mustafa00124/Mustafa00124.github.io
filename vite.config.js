ea8da54096b4b5697569815fc7db92dc8f7f80c2
Mustafa <23100124@lums.edu.pk>
2025-10-18 20:01:27 +0500

    Fix MIME type issues for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,13 @@ export default defineConfig({
   base: '/Mustafa00124.github.io/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets'
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
   }
 })
